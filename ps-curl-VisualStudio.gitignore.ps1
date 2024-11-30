65f9393687bcf89ae607877f61e999450e471c65
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-11-30 21:30:51 +0700

    0.0.4

---
+++ ps-curl-VisualStudio.gitignore.ps1
@@ -0,0 +1,2 @@
+# Download the Visual Studio .gitignore file from GitHub and save it as .gitignore
+curl https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore -o .gitignore
